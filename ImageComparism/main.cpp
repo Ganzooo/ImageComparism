90c71563a2b8412c4b463362f56342f9e4d5bead
gnzrg25@gmail.com <gnzrg25@gmail.com>
2018-03-27 14:47:50 +0900

    Initial Commit

---
+++ ImageComparism/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageComparism
+//
+//  Created by Ganzorig Gankhuyag on 27/03/2018.
+//  Copyright © 2018 Ganzorig Gankhuyag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
